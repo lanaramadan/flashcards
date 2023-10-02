1bfeab55bd58c0c58f1c9c26b870f42919748733
Lana Ramadan <lanamramadan@gmail.com>
2023-10-02 03:39:20 -0700

    Ability to guess answer + streaks
    Adding an input and submit button so users can guess before viewing the answer. Also, ability to view streaks.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -10,11 +10,26 @@ const Card = (props) => {
 
     };
 
+    const [questionIndex, setQuestionIndex] = useState(0);
+
+    const [streak, setStreak] = useState(0);
+    const [longestStreak, setLongestStreak] = useState(0);
+
+    // alternates between "question" and "answer"
     const [currentSide, setCurrentSide] = useState("question");
+
+    // alternates between "none" and "correct" and "incorrect"
+    const [currentGuessType, setCurrentGuessType] = useState("none");
+
     const [raw_type, raw_question, raw_answer] = getQuestion();
     const [type, setType] = useState(raw_type);
     const [question, setQuestion] = useState(raw_question);
     const [answer, setAnswer] = useState(raw_answer);
+    const [guess, setGuess] = useState("");
+
+    const previousQuestion = () => {
+
+    };
 
     const newQuestion = () => {
         const [raw_type, raw_question, raw_answer] = getQuestion();
@@ -22,19 +37,60 @@ const Card = (props) => {
         setQuestion(raw_question);
         setAnswer(raw_answer);
         setCurrentSide("question")
+        setCurrentGuessType("none")
+        setGuess('')
     };
 
     const switchSide = () => {setCurrentSide(currentSide === "question" ? "answer" : "question")};
 
-    
+    const handleGuess = (e) => {setGuess(e.target.value)};
+    const checkGuess = (answer) => {
+        if (guess.toLowerCase() === answer.toLowerCase()) {
+            setCurrentGuessType("correct")
+            setStreak(streak + 1)
+        }
+        else {
+            setCurrentGuessType("incorrect")
+            if (streak > longestStreak) {
+                setLongestStreak(streak)
+            }
+            setStreak(0)
+        }
+    };
+
     return (
-        <div className="card-mechanism">
-            <div className={`content ${type}`} onClick={switchSide}>
-                <h2>{currentSide === "question" ? question : answer}</h2>
+        <div>
+            <div className="streaks">
+                <h4 className="streak">Current Streak: {streak}</h4>
+                <h4 className="streak">Longest Streak: {longestStreak}</h4>
             </div>
 
-            <div className="arrow">
-                <button onClick={newQuestion}>⭢</button>
+            <div className="card-mechanism">
+                <div className={`content ${type}`} onClick={switchSide}>
+                    <h2>{currentSide === "question" ? question : answer}</h2>
+                </div>
+
+                <div className="card-guess">
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Place your answer here..."
+                            onChange={handleGuess}
+                            className ={`textbox ${currentGuessType}`}
+                            value={guess}
+                        />
+
+                        <button type="button" className="button submit" onClick={() => checkGuess(answer)}>
+                            Submit Guess
+                        </button>
+                    </form>
+
+                </div>
+
+                <div className="arrow">
+                    <button onClick={previousQuestion}>←</button>
+                    <button onClick={newQuestion}>→</button>
+                </div>
             </div>
         </div>
     )
